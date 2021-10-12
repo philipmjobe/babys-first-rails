e564e4caddea1efbd9338687235eddac539d3e6c
Philip <philipmjobe@gmail.com>
2021-10-12 17:33:11 -0400

    added new get method to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get "/welcome_path" => "welcomes#hello_method"
+get "/about_path" => "welcomes#about_method"
 end
