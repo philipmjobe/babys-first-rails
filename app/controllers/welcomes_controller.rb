b14d5cec2dd80826e6aa67e351751575a363331f
Philip <philipmjobe@gmail.com>
2021-10-12 17:35:34 -0400

    added def about_method to welcomes_controller.rb

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,4 +2,9 @@ class WelcomesController < ApplicationController
   def hello_method
     render json: {message: "Hello from welcomes controller!"}
   end
+
+  def about_method
+    render json: {message: "My favorite language is Ruby!"}
+  end
+  
 end
